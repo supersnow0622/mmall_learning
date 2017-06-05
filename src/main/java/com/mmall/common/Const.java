342452f15316759eefeba30cc896fb3027605c29
SherryWang <SherryWang>
2017-06-05 20:51:14 +0800

    User and Category

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,19 @@
+package com.mmall.common;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+
+/**
+ * Created by Yong_2 on 2017/6/4.
+ */
+public class Const {
+
+    public static final String CURRENT_USER = "currentUser";
+
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+
+    public interface Role{
+        int ROLE_CUSTOMER = 0;//普通用户
+        int ROLE_ADMIN = 1;//管理员
+    }
+}
---
+++ src/main/java/com/mmall/util/PropertiesUtil.java
@@ -0,0 +1,7 @@
+package com.mmall.util;
+
+/**
+ * Created by Yong_2 on 2017/6/4.
+ */
+public class PropertiesUtil {
+}
